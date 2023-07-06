8138e51a3ea9fc70951e14b91fa180898664a91f
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-06 22:59:59 +0900

    New 2カラムのチェックボックスエリアコンポーネントの作成

---
+++ frontend/src/components/presantation/top/PurposeForm.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Checkbox } from "../../ui/index"
+import { Purpose } from "../../types/types"
+import classes from "./PurposeForm.module.css"
+
+interface PurposeList {
+    purposes: Purpose[]
+}
+
+const PurposeForm = (props: PurposeList) => {
+    return (
+        <div className={classes.container}>
+            {props.purposes.map(purpose =>
+                <Checkbox key={purpose.id} style={{ width: "calc(100%/2)" }} {...purpose}/> 
+            )}
+        </div>
+    )
+}
+
+export default PurposeForm
