fabd986aad6e1d05973a4222fbc488db60d270df
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-06 22:57:20 +0900

    change ・input要素に属性追加 ・labelのfor属性の変更

--- frontend/src/components/ui/checkbox/Checkbox.tsx
+++ frontend/src/components/ui/checkbox/Checkbox.tsx
@@ -4,13 +4,20 @@ import classes from "./checkbox.module.css"
 interface Props {
     id: string;
     labelName: string;
+    checked: boolean;
+    value: string;
 };
 
 const Checkbox = (props: Props) => {
     return (
-        <div className={classes.checkboxArea}>
-            <input type="checkbox" name={props.labelName} id= {props.id} />
-            <label className={classes.checkboxLabelText} htmlFor={props.labelName}>
+            <input  type="checkbox" 
+                    name={props.labelName} 
+                    id= {props.id} 
+                    value={props.value} 
+                    checked={props.checked}
+                    className={classes.checkbox}
+            />
+            <label className={classes.checkboxLabelText} htmlFor={props.id}>
                 { props.labelName }
             </label>
         </div>
