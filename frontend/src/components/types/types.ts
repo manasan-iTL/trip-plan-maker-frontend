5c6fbf5f757e600a7ea29434f534662bc818551f
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-12 23:40:25 +0900

    add TabActiveの型を追加 ・TabActive SPOT or AREAを取る型

--- frontend/src/components/types/types.ts
+++ frontend/src/components/types/types.ts
@@ -1,7 +1,28 @@
+type Prefecture = {
+    id: string;
+    value: string;
+    checked: boolean;
+}
+
+type Category = {
+    id: string;
+    value: string;
+    checked: boolean
+}
+
+type AccordionChild = Prefecture | Category
+
+export type TabActive = "SPOT" | "AREA"
 
 export interface Purpose {
     id: string;
     labelName: string;
     checked: boolean;
     value: string;
+}
+
+export interface Accordion {
+    titleName: string;
+    active: boolean;
+    childItems: AccordionChild[]
 }
