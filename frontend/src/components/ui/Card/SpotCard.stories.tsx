597fe2cd2163dfb9d2de5872803cd4cdf6f95d5d
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-12 23:28:01 +0900

    Test スポットカードコンポーネントのテスト追加

---
+++ frontend/src/components/ui/Card/SpotCard.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SpotCard from "./SpotCard";
+
+const meta = {
+    title: "Card/SpotCard",
+    component: SpotCard
+} satisfies Meta<typeof SpotCard>
+
+export default meta
+
+type Story = StoryObj<typeof SpotCard>
+
+export const Default: Story = {
+    args: {
+        spotName: "東京スカイツリー",
+        spotImgAlt: "東京スカイツリー",
+        spotImgSrc: "https://www.tokyo-skytree.jp/img/lighting/db7bb7802658fefbdcae4ca407cefd4632233320221406389.png",
+    }
+}
