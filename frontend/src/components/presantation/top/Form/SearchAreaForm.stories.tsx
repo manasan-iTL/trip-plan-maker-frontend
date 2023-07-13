4ba067d9448894429c1a41fde365841b5ab46f15
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-13 23:43:21 +0900

    Test タブで入力フォームを切り替えるコンポーネントのテスト追加

--- frontend/src/components/presantation/top/Form/SearchAreaForm.stories.tsx
+++ frontend/src/components/presantation/top/Form/SearchAreaForm.stories.tsx
@@ -12,12 +12,50 @@ type Story = StoryObj<typeof SearchAreaForm>
 
 export const SearchSpot: Story = {
     args: {
-        TabActive: "SPOT"
+        TabActive: "SPOT",
+        spotInputId: "sample1",
+        spotValue: "",
+        spots: [
+            {
+                spotName: "東京スカイツリー",
+                spotImgAlt: "東京スカイツリー",
+                spotImgSrc: "https://www.tokyo-skytree.jp/img/lighting/db7bb7802658fefbdcae4ca407cefd4632233320221406389.png"
+            }
+        ],
+        accordionItems: [
+            { 
+                titleName: "東北", 
+                active: true,
+                childItems: [
+                    { id:"miyagi", value: "宮城", checked: true },
+                    { id:"aomori", value: "青森", checked: false }
+                ] 
+            },
+        ]
     }
 }
 
 export const SearchArea: Story = {
     args: {
-        TabActive: "AREA"
+        TabActive: "AREA",
+        spotInputId: "sample1",
+        spotValue: "",
+        spots: [
+            {
+                spotName: "東京スカイツリー",
+                spotImgAlt: "東京スカイツリー",
+                spotImgSrc: "https://www.tokyo-skytree.jp/img/lighting/db7bb7802658fefbdcae4ca407cefd4632233320221406389.png"
+            }
+        ],
+        accordionItems: [
+            { 
+                titleName: "東北", 
+                active: true,
+                childItems: [
+                    { id:"miyagi", value: "宮城", checked: true },
+                    { id:"aomori", value: "青森", checked: false }
+                ] 
+            },
+        ]
     }
 }
