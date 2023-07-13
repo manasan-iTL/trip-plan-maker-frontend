c6bca7e718a1604a1bc0a8124379274bd1957a0c
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-13 23:43:32 +0900

    change インポート方法を変更

--- frontend/src/components/presantation/top/Form/PurposeForm.tsx
+++ frontend/src/components/presantation/top/Form/PurposeForm.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Checkbox } from "../../../ui/index"
+import { Checkbox } from "../../../ui"
 import { Purpose } from "../../../types/types"
 import classes from "./PurposeForm.module.css"
 
