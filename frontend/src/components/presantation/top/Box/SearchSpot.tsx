b0cc3314d05d1fd0f3c62b26fc5b48728936498c
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-13 23:41:08 +0900

    New SearchSpotコンポーネントの追加

---
+++ frontend/src/components/presantation/top/Box/SearchSpot.tsx
@@ -0,0 +1,31 @@
+import { Spot } from "../../../types/types";
+import { SpotCard, TextInput } from "../../../ui";
+import classes from "./SearchSpot.module.css"
+
+interface Props {
+    inputId: string;
+    inputValue: string;
+    spots: Spot[];
+}
+
+const SearchSpot = (props: Props) => {
+    return (
+        <div className={classes.container}>
+            <div>
+                <p className={classes.labelArea}>
+                    <label className={classes.heading} htmlFor={props.inputId}>行きたい場所</label>
+                    <span className={classes.subText}>例　東京スカイツリー</span>
+                </p>
+                <TextInput id={props.inputId} value={props.inputValue} iconSize="20px"/>
+            </div>
+            <div>
+                <p className={classes.heading}>検索結果</p>
+                { props.spots.map(spot => (
+                    <SpotCard spotName={spot.spotName} spotImgAlt={spot.spotImgAlt} spotImgSrc={spot.spotImgSrc}/>
+                )) }
+            </div>
+        </div>
+    )
+}
+
+export default SearchSpot
