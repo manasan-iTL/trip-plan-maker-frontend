e6d3cc815ab8fa5d04c257ec7aa285868ee9e018
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-13 23:40:05 +0900

    add コンポーネントのエントリーを追加

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,3 +1,7 @@
 
 export { default as Checkbox } from "./checkbox/Checkbox";
-export { default as Tab } from "./Tab/Tab"
+export { default as Tab } from "./Tab/Tab";
+export { default as TextInput } from "./Input/TextInput";
+export { default as SpotCard } from "./Card/SpotCard";
+export { default as AccordionItem } from "./Accordion/AccordionItem"
+export { default as Button } from "./Button/Button"
