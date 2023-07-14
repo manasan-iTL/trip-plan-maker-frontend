4be25f662bd8316876b1bf840e36ee97b4fc9f5d
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-14 18:38:17 +0900

    add EmptyCardコンポーネントをエントリー追加

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -3,5 +3,6 @@ export { default as Checkbox } from "./checkbox/Checkbox";
 export { default as Tab } from "./Tab/Tab";
 export { default as TextInput } from "./Input/TextInput";
 export { default as SpotCard } from "./Card/SpotCard";
+export { default as EmptyCard } from "./Card/EmptyCard"
 export { default as AccordionItem } from "./Accordion/AccordionItem"
 export { default as Button } from "./Button/Button"
