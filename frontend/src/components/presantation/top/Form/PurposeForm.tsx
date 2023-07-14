d563b69656a869ed12f90cdb1566d249c62106cd
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-14 18:37:04 +0900

    change 見出しともっと見るリンクを追加

--- frontend/src/components/presantation/top/Form/PurposeForm.tsx
+++ frontend/src/components/presantation/top/Form/PurposeForm.tsx
@@ -10,9 +10,19 @@ interface PurposeList {
 const PurposeForm = (props: PurposeList) => {
     return (
         <div className={classes.container}>
-            {props.purposes.map(purpose =>
-                <Checkbox key={purpose.id} style={{ width: "calc(100%/2)" }} {...purpose}/> 
-            )}
+            <div>
+                <h2 className={classes.heading}>目的（複数選択可）</h2>
+            </div>
+            <div className={classes.checkboxArea}>
+                {props.purposes.map(purpose =>
+                    <Checkbox key={purpose.id} style={{ width: "calc(100%/2)" }} {...purpose}/> 
+                )}
+            </div>
+            <p className={classes.more}>
+                <a href="#" className={classes.moreText}>
+                    もっと見る
+                </a>
+            </p>
         </div>
     )
 }
