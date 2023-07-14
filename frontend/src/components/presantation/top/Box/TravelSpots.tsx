ff479ef6b6fa00d03574a25f6f7cdf93ec6ae867
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-14 21:18:46 +0900

    New 行きたい場所コンポーネントの追加

---
+++ frontend/src/components/presantation/top/Box/TravelSpots.tsx
@@ -0,0 +1,22 @@
+import { Spot } from "../../../types/types"
+import { EmptyCard, SpotCard } from "../../../ui"
+import classe from "./TravelSpots.module.css"
+
+interface Props {
+    spots: Spot[]
+}
+
+const TravelSpots = (props: Props) => {
+    return (
+        <div className={classe.container}>
+            <h2 className={classe.heading}>行きたい場所</h2>
+            <div>
+                { !props.spots.length ? <EmptyCard text="１つ以上は選択してください" iconNumber={1} /> 
+                                     : props.spots.map(spot => <SpotCard {...spot}/>)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default TravelSpots
