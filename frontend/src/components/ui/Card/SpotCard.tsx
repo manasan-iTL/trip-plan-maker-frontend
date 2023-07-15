9049522553eec410b14899ebfca2781c766c966f
manasan-iTL <daiyaofssh2020@gmail.com>
2023-07-15 23:36:35 +0900

    change Propsの型変更 ・stylesの型を追加

--- frontend/src/components/ui/Card/SpotCard.tsx
+++ frontend/src/components/ui/Card/SpotCard.tsx
@@ -1,14 +1,15 @@
+import React from "react";
 import classes from "./SpotCard.module.css"
 import { Spot } from "../../types/types";
 import Button from "../Button/Button";
 
-type Props = Spot
+type Props = Spot & { imgSize: React.CSSProperties }
 
 const SpotCard = (props: Props) => {
     return (
         <div className={classes.container}>
             <div className={classes.imgArea}>
-                <img src={props.spotImgSrc} alt={props.spotImgAlt} className={classes.img}/>
+                <img src={props.spotImgSrc} alt={props.spotImgAlt} className={classes.img} style={props.imgSize}/>
             </div>
             <div className={classes.spot}>
                 <p className={classes.text}>{props.spotName}</p>
