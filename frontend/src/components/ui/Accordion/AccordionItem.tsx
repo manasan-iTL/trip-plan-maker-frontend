53a4f061669251cf4e0026f9f087cd867a53efda
manasan-iTL <daiyaofssh2020@gmail.com>
2024-02-01 20:20:00 +0900

    chore アコーディオン開閉ができるよう状態と更新関数を追加

--- frontend/src/components/ui/Accordion/AccordionItem.tsx
+++ frontend/src/components/ui/Accordion/AccordionItem.tsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { LuMinusCircle, LuPlusCircle } from "react-icons/lu"
 import classes from "./AccordionItem.module.css"
 
 interface Props {
     categoryName: string;
-    active: boolean;
     children: React.ReactNode
 }
 
 const AccordionItem = (props: Props) => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const handleClick = () => {
+        setIsOpen(preState => !preState)
+    }
+
     return (
         <section className={classes.container}>
-            <div className={classes.titleArea}>
+            <div className={classes.titleArea} onClick={handleClick}>
                 <h4>{props.categoryName}</h4>
-                {props.active? <LuMinusCircle size={"20px"}/> :<LuPlusCircle size={"20px"}/>}
+                {isOpen? <LuMinusCircle size={"20px"}/> :<LuPlusCircle size={"20px"}/>}
             </div>
-            {props.active? props.children: null}
+            <div className={classes.initial + " " + (isOpen ? classes.open: "")}>
+                {isOpen? props.children: null}
+            </div>
         </section>
     )
 }
