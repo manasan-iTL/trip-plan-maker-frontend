430716a0a089909122ff710b17b20c0f48a257e5
manasan-iTL <daiyaofssh2020@gmail.com>
2024-02-06 20:37:00 +0900

    chore uiコンポーネントのexport先を変更

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -3,6 +3,8 @@ export { default as Checkbox } from "./checkbox/Checkbox";
 export { default as Tab } from "./Tab/Tab";
 export { default as TextInput } from "./Input/TextInput";
 export { default as SpotCard } from "./Card/SpotCard";
+export { default as WayPointCard } from "./Card/WayPointCard";
+export { default as FixedPointCard } from "./Card/FixedPointCard";
 export { default as EmptyCard } from "./Card/EmptyCard"
 export { default as AccordionItem } from "./Accordion/AccordionItem"
 export { default as Button } from "./Button/Button"
