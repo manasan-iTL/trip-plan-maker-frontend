63c6f376b6178f1ed8208c7ba34236182c58d68c
manasan-iTL <daiyaofssh2020@gmail.com>
2024-02-07 11:19:00 +0900

    chore interfaceにonChange等を追加

--- frontend/src/components/presantation/top/Accordions.tsx
+++ frontend/src/components/presantation/top/Accordions.tsx
@@ -3,7 +3,8 @@ import { AccordionItem, Checkbox } from "../../ui"
 import classes from "./Accordions.module.css"
 
 interface Props {
-    accordionItems: Accordion[],
+    accordionItems: Accordion[];
+    onChangeCheckbox: (value: string) => void;
 }
 
 const Accordions = (props: Props) => {
@@ -11,13 +12,14 @@ const Accordions = (props: Props) => {
         <div className={classes.container}>
             { props.accordionItems && 
               props.accordionItems.map(item => (
-                <AccordionItem key={item.titleName} categoryName={item.titleName} active={item.active} >
+                <AccordionItem key={item.titleName} categoryName={item.titleName} >
                     { item.childItems.map(childItem => 
                         <Checkbox id={childItem.id} 
                                   labelName={childItem.value} 
                                   value={childItem.value} 
-                                  style={{}} 
                                   checked={childItem.checked}
+                                  onChange={() => props.onChangeCheckbox(childItem.value)}
+                                  style={{}} 
                                   key={childItem.id}
                         />
                     ) }
