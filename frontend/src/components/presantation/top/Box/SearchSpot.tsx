1c8d284ed7e4173d1ebb000e995e88979fefa713
manasan-iTL <daiyaofssh2020@gmail.com>
2024-02-08 11:29:00 +0900

    chore 固定値部分を汎用コンポーネントに置き換え

--- frontend/src/components/presantation/top/Box/SearchSpot.tsx
+++ frontend/src/components/presantation/top/Box/SearchSpot.tsx
@@ -1,27 +1,37 @@
+import React from "react";
 import { Spot } from "../../../types/types";
 import { SpotCard, TextInput } from "../../../ui";
 import classes from "./SearchSpot.module.css"
 
 interface Props {
-    inputId: string;
     inputValue: string;
-    spots: Spot[];
+    spots: Spot[] | undefined;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    searchBtnClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+    handleAddSpot: (e: React.MouseEvent<HTMLButtonElement>, spotName: string) => void
 }
 
 const SearchSpot = (props: Props) => {
+
     return (
         <div className={classes.container}>
             <div>
                 <p className={classes.labelArea}>
-                    <label className={classes.heading} htmlFor={props.inputId}>行きたい場所</label>
+                    <label className={classes.heading}>行きたい場所</label>
                     <span className={classes.subText}>例　東京スカイツリー</span>
                 </p>
-                <TextInput id={props.inputId} value={props.inputValue} iconSize="20px"/>
+                <TextInput value={props.inputValue} iconSize="20px" onChange={props.onChange} onClick={props.searchBtnClick}/>
             </div>
             <div>
                 <p className={classes.heading}>検索結果</p>
-                { props.spots.length? props.spots.map(spot => (
-                    <SpotCard spotName={spot.spotName} spotImgAlt={spot.spotImgAlt} spotImgSrc={spot.spotImgSrc} imgSize={{height: "70px"}}/>
+                { props.spots?.length? props.spots.map(spot => (
+                    <SpotCard 
+                        {...spot}
+                        imgSize={{height: "70px"}}
+                        onClick={(e: React.MouseEvent<HTMLButtonElement>) => props.handleAddSpot(e, spot.spotName)}
+                        key={spot.spotName}
+                        text="追加"
+                    />
                 )) 
                 :null
                 }
