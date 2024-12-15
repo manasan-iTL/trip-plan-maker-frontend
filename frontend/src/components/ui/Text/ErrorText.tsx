8a6d7441cc77566a15e48396f72ce23e5868f7ac
manasan-iTL <daiyaofssh2020@gmail.com>
2024-12-16 03:20:55 +0900

    change: エラーテキストのフォントサイズを任意で変更できるように

--- frontend/src/components/ui/Text/ErrorText.tsx
+++ frontend/src/components/ui/Text/ErrorText.tsx
@@ -3,12 +3,13 @@ import classes from "./ErrorText.module.css"
 
 interface Props {
     text: string;
+    fontSize?: number;
 }
 
 const ErrorText = (props: Props) => {
 
     return (
-        <small className={classes.text}>
+        <small className={classes.text} style={{fontSize: props.fontSize ? `${props.fontSize}px`: '14px'}}>
             {props.text}
         </small>
     )
