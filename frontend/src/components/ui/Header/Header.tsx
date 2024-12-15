4f0f687d269bcd758789a168a984550ff939ddf8
manasan-iTL <daiyaofssh2020@gmail.com>
2024-12-16 04:45:43 +0900

    fix: 要らない要素を削除

--- frontend/src/components/ui/Header/Header.tsx
+++ frontend/src/components/ui/Header/Header.tsx
@@ -10,20 +10,6 @@ const Header = () => {
                         <img src={logo} alt="" className={classes.image}/>
                     </a>
                 </h1>
-                <nav className={classes.globalNav}>
-                    <ul className={classes.list}>
-                        <li className={classes.item}>
-                            <a href="#" className={classes.link}>
-                                このサイトについて
-                            </a>
-                        </li>
-                        <li className={classes.item}>
-                            <a href="#" className={classes.link}>
-                                会員登録
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </header>
     )
